0b50842d8e9c7948ed9ec94ca5b0f219804f8fe7
Soham Kale <skale1@berkeley.edu>
2020-02-09 14:01:32 -0800

    fixed build issue?

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Result, Affix } from "antd";
+import { Button, Result } from "antd";
 import { Link } from "react-router-dom";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
